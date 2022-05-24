21788e0eabdfe5948bc9acb24d9f9dd72a3c6d9c
Mohamed <m00ibrahim257@gmail.com>
2022-05-24 11:05:31 +0200

    second

--- technext.github.io/fox/js/slider.js
+++ technext.github.io/fox/js/slider.js
@@ -1,5 +1,5 @@
 // $(document).ready(function () {
-//   $(".customer-logos").slick({
+//   $(".customer-pic").slick({
 //     slidesToShow: 6,
 //     slidesToScroll: 1,
 //     autoplay: true,
@@ -27,72 +27,72 @@
 
 
 
-const slide = {
-  main : null,
-  elementImg : null,
-  imgSelected : 0,
-  nextSlide: function (){
-      if (this.imgSelected != null)
-      {
-          if (this.imgSelected < (this.elementImg.length - 1))
-          {
-              this.imgSelected++;
-              this.normalizeSlide();
-          }
-      }
-  },
-  prevSlide: function (){
-      if (this.imgSelected != null)
-      {
-          if (this.imgSelected > 0)
-          {
-              this.imgSelected--;
-              this.normalizeSlide();
-          }
-      }
-  },
-  normalizeSlide: function (){
+// const slide = {
+//   main : null,
+//   elementImg : null,
+//   imgSelected : 0,
+//   nextSlide: function (){
+//       if (this.imgSelected != null)
+//       {
+//           if (this.imgSelected < (this.elementImg.length - 1))
+//           {
+//               this.imgSelected++;
+//               this.normalizeSlide();
+//           }
+//       }
+//   },
+//   prevSlide: function (){
+//       if (this.imgSelected != null)
+//       {
+//           if (this.imgSelected > 0)
+//           {
+//               this.imgSelected--;
+//               this.normalizeSlide();
+//           }
+//       }
+//   },
+//   normalizeSlide: function (){
 
-      for (num = 0; num < this.elementImg.length; num++)
-      {
-          this.elementImg[num].classList.remove("hideLeft","prevLeftSecond","prev","selected","next","nextRightSecond","hideRight");
-      }
+//       for (num = 0; num < this.elementImg.length; num++)
+//       {
+//           this.elementImg[num].classList.remove("hideLeft","prevLeftSecond","prev","selected","next","nextRightSecond","hideRight");
+//       }
 
-      this.elementImg[this.imgSelected].classList.add("selected");
+//       this.elementImg[this.imgSelected].classList.add("selected");
 
-      if (this.imgSelected > 2)
-      {
-          this.elementImg[this.imgSelected-2].classList.add("hideLeft");
-          this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
-          this.elementImg[this.imgSelected-1].classList.add("prev");
-      }
-      else if (this.imgSelected > 1)
-      {
-          this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
-          this.elementImg[this.imgSelected-1].classList.add("prev");
-      }
-      else if (this.imgSelected > 0)
-      {
-          this.elementImg[this.imgSelected-1].classList.add("prev");
-      }
+//       if (this.imgSelected > 2)
+//       {
+//           this.elementImg[this.imgSelected-2].classList.add("hideLeft");
+//           this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
+//           this.elementImg[this.imgSelected-1].classList.add("prev");
+//       }
+//       else if (this.imgSelected > 1)
+//       {
+//           this.elementImg[this.imgSelected-2].classList.add("prevLeftSecond");
+//           this.elementImg[this.imgSelected-1].classList.add("prev");
+//       }
+//       else if (this.imgSelected > 0)
+//       {
+//           this.elementImg[this.imgSelected-1].classList.add("prev");
+//       }
 
-      if ((this.imgSelected + 3) < this.elementImg.length)
-      {
-          this.elementImg[this.imgSelected+3].classList.add("hideRight");
-          this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
-          this.elementImg[this.imgSelected+1].classList.add("next");
-      }
-      else if ((this.imgSelected + 2) < this.elementImg.length)
-      {
-          this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
-          this.elementImg[this.imgSelected+1].classList.add("next");
-      }
-      else if((this.imgSelected + 1) < this.elementImg.length)
-      {
-          this.elementImg[this.imgSelected+1].classList.add("next");
-      }
-  }
-}
+//       if ((this.imgSelected + 3) < this.elementImg.length)
+//       {
+//           this.elementImg[this.imgSelected+3].classList.add("hideRight");
+//           this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
+//           this.elementImg[this.imgSelected+1].classList.add("next");
+//       }
+//       else if ((this.imgSelected + 2) < this.elementImg.length)
+//       {
+//           this.elementImg[this.imgSelected+2].classList.add("nextRightSecond");
+//           this.elementImg[this.imgSelected+1].classList.add("next");
+//       }
+//       else if((this.imgSelected + 1) < this.elementImg.length)
+//       {
+//           this.elementImg[this.imgSelected+1].classList.add("next");
+//       }
+//   }
+// }
 
 
 
@@ -118,7 +118,7 @@ $(document).ready(function(){
     slidesToShow: 6,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 1000,
+    autoplaySpeed: 700,
     arrows: false,
     dots: false,
       pauseOnHover: false,
